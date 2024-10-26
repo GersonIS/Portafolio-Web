03c8d37056ab5b421e676ec4e9890507e5e2f6d7
GersonIS <gersondelacruz455@gmail.com>
2024-10-26 09:54:48 -0500

    Seleccionando proyectos y agregando funcionalidad a contacto

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,8 +1,29 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 
 const ContactSection: React.FC = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const { name, email, message } = form;
+    window.open(
+      `https://wa.me/948765692?text=Nombre:%20${name}%0ACorreo:%20${email}%0AMensaje:%20${message}`
+    );
+    setForm({ name: "", email: "", message: "" });
+  };
+
   return (
     <section id="contact" className="min-h-screen bg-gray-900 text-white p-10">
       <h2 className="text-4xl font-bold text-center mb-10 bg-clip-text text-transparent bg-gradient-to-r from-teal-400 via-green-400 to-blue-500">
@@ -14,20 +35,38 @@ const ContactSection: React.FC = () => {
           cybersecurity. Feel free to reach out if you&apos;d like to
           collaborate or have any questions.
         </p>
-        <form className="grid grid-cols-1 gap-6">
+        <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-6">
           <input
             type="text"
             placeholder="Your Name"
+            id="name"
+            name="name"
+            value={form.name}
+            onChange={handleChange}
+            required
+            autoComplete="off"
             className="p-4 rounded-lg bg-gray-700 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-teal-400"
           />
           <input
             type="email"
             placeholder="Your Email"
+            id="email"
+            name="email"
+            value={form.email}
+            onChange={handleChange}
+            required
+            autoComplete="off"
             className="p-4 rounded-lg bg-gray-700 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-teal-400"
           />
           <textarea
             placeholder="Your Message"
+            id="message"
+            name="message"
+            value={form.message}
+            onChange={handleChange}
             rows={5}
+            required
+            autoComplete="off"
             className="p-4 rounded-lg bg-gray-700 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-teal-400"
           ></textarea>
           <motion.button
